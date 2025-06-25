9729f3790515f6c66df811479725901c4f92c752
Dharmendra songira <157127103+dharmendrasongira@users.noreply.github.com>
2025-06-25 15:25:07 +0530

    Time: 0 ms (100%), Space: 43.2 MB (50.03%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,21 +1,12 @@
 class Solution {
-    public int rec(int[] cost, int i,int dp[]) {
-        //base case
-        if (i <= 1) {
-            return cost[i];
-        }
-        if(dp [i] != -1)return dp[i];
-        //logic
-
-        return dp[i]= cost[i] + Math.min(rec(cost, i - 1,dp), rec(cost, i - 2,dp));
-    }
-
     public int minCostClimbingStairs(int[] cost) {
-
-        int i = cost.length - 1;
-        int []dp =new int [cost.length];
-        Arrays.fill(dp ,-1);
-
-        return Math.min(rec(cost, i,dp), rec(cost, i - 1,dp));
+        int n = cost.length;
+         int dp []= new int[n];
+         dp[0]= cost[0];
+         dp[1] = cost[1];
+         for (int i =2; i<n;i++){
+              dp [i] = cost[i]+ Math.min(dp[i-2],dp[i-1]);
+         }
+         return Math.min(dp[n-1],dp[n-2]);
     }
 }
