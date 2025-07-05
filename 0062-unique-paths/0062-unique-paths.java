234b31c2335b2800ed2cef66aa85befba23d2a3a
Dharmendra songira <157127103+dharmendrasongira@users.noreply.github.com>
2025-07-05 18:46:37 +0530

    Time: 0 ms (100%), Space: 40.2 MB (95.28%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,18 @@
+class Solution {
+ 
+    public int rec(int m, int n, int [][]dp) {
+        if(m==0 && n ==0) return 1;
+       if(m < 0 || n < 0) return 0; 
+       if(dp[m][n] != -1) return dp[m][n];
+        return dp[m][n] = rec(m-1, n,dp)+ rec(m,n-1,dp);
+
+    }
+    public int uniquePaths(int m, int n) {
+           int [][ ]dp =new int[m+1][n+1];
+          for (int[] row : dp) {
+    Arrays.fill(row, -1);
+}
+         return rec(m-1, n-1 ,dp);
+
+    }
+}
